879f1bcdafdee8e9dc19425cfa4627f4c84008fb
Sarah Gray <sgray@neat.com>
2013-03-11 14:24:19 -0700

    changing word placeholder to easier reading

--- haiku.py
+++ haiku.py
@@ -2,14 +2,14 @@
 
 import random
 
-mad_lib_0 = ("Haikus are %0, but sometimes they don't make %1, %2",
+mad_lib_0 = ("Haikus are WORD#0, but sometimes they don't make WORD#1, WORD#2",
             ["a one syllable adjective", "a one syllable noun", "a five syllable noun"])
 
-mad_lib_1 = ("Out of %0, We wish to %1 the whole %2, But we never will.",
+mad_lib_1 = ("Out of WORD#0, We wish to WORD#1 the whole WORD#2, But we never will.",
             ["a three syllable noun", "a one syllable verb", "a one syllable noun"])
 
-mad_lib_2 = ("Lightening %0 -- what I thought were %1, are plumes of %2",
-            ["a one syllable adjective", "a two syllable plural noun", "a three syllable noun"])
+mad_lib_2 = ("Lightening WORD#0 -- what I thought were WORD#1, are plumes of pampas grass",
+            ["a one syllable verb", "a two syllable plural noun"])
 
 list_of_mad_libs = [mad_lib_0, mad_lib_1, mad_lib_2]
 
@@ -20,7 +20,7 @@ cues_for_reader = mad_lib[1]
 
 for i in range(len(cues_for_reader)):
     response = raw_input("Enter " + cues_for_reader[i] + ": ")
-    haiku = haiku.replace("%" + str(i), response)
+    haiku = haiku.replace("WORD#" + str(i), response)
 
 print haiku
 
