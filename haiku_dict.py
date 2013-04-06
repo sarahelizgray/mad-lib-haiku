b451e66d5f34bee78218b103f711f74e65c6c981
Sarah Gray <sgray@neat.com>
2013-04-06 11:53:05 -0400

    adding space around braces for readability

--- haiku_dict.py
+++ haiku_dict.py
@@ -1,13 +1,17 @@
 import random
 
-dict_of_mad_libs = {"Haikus are WORD#0, but sometimes they don't make WORD#1, WORD#2" :
+dict_of_mad_libs = {
+
+            "Haikus are WORD#0, but sometimes they don't make WORD#1, WORD#2" :
             ["a one syllable adjective", "a one syllable noun", "a five syllable noun"],
 
             "Out of WORD#0, We wish to WORD#1 the whole WORD#2, But we never will." :
             ["a three syllable noun", "a one syllable verb", "a one syllable noun"],
 
             "Lightening WORD#0 -- what I thought were WORD#1, are plumes of pampas grass" :
-            ["a one syllable verb", "a two syllable plural noun"]}
+            ["a one syllable verb", "a two syllable plural noun"]
+
+            }
 
 
 haiku = random.choice(dict_of_mad_libs.keys())
